9e4b7471a17ba87e2ef9057a9d46880a93035acb
Aukan666 <emoroni.villadelobos@gmail.com>
2024-05-05 22:36:08 -0400

    Agrega archivos iniciales del proyecto

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+let intentos = 0;
+
+function verificarIntento() {
+    const intento = parseInt(document.getElementById('intent').value);
+
+    if (isNaN(intento) || intento < 1 || intento > 100) {
+        establecerMensaje('Por favor, ingresa un número válido entre 1 y 100.');
+        return;
+    }
+
+    intentos++;
+
+    if (intento === numeroAleatorio) {
+        establecerMensaje(`¡Acertaste! Solo utilizaste ${intentos} intentos.`);
+    } else if (intento < numeroAleatorio) {
+        establecerMensaje('No, mi número es mayor.');
+    } else {
+        establecerMensaje('No, mi número es menor.');
+    }
+}
+
+function establecerMensaje(mensaje) {
+    document.getElementById('mensaje').textContent = mensaje;
+}
